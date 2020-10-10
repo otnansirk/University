9c66f98d94da98f42fa4c4674ad0871c8f3cac4a
kris <kris@gmail.com>
2020-10-11 00:19:20 +0700

    buat form pertemuan ke 4

--- Teknologi Mobile/HelloWordsKotlin/app/src/main/java/com/example/hellowordskotlin/MainActivity.kt
+++ Teknologi Mobile/HelloWordsKotlin/app/src/main/java/com/example/hellowordskotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.hellowordskotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val add_btn = findViewById<Button>(R.id.btn_add)
+        add_btn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Berhasil", Toast.LENGTH_LONG).show()
+        }
+        val close_btn = findViewById<Button>(R.id.btn_close)
+        close_btn.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Anda menekan tombol close", Toast.LENGTH_LONG).show()
+        }
+
     }
 }
