b3c96cdec8ec8fa207d9749206934e630c2e4242
kris <kris@gmail.com>
2020-12-10 14:38:48 +0700

    Pertemuan 8 -> TekMobile

---
+++ Pertemuan8/app/src/main/java/com/pertemuan8/ViewPagerAdapter.kt
@@ -0,0 +1,33 @@
+package com.pertemuan8
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter(private val mContext: Context, fm: FragmentManager) :
+        FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0) {
+            FirstFrament.newInstance()
+        } else {
+            SecondFragment.newInstance()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.tab_text_1,
+                R.string.tab_text_2)
+    }
+}
